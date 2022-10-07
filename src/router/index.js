1007b2465b27a86683a5d3b340ff0875b26c4f5c
kshitij2022 <kshitijwohlig@gmail.com>
2022-10-07 14:29:02 +0530

    deploy

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Todo from "../views/Todo.vue";
+import About from "../views/About.vue";
+import Photos from "../views/Photos.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Todo",
+    component: Todo,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/photos",
+    name: "Photos",
+    component: Photos,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
